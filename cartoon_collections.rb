8546576679c7fae8e9432ce71bc95a598c185c77
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-07 23:56:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves (array) # code an argument here
+    array.map.with_index(1) { |name, index| puts "#{index}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array) # code an argument here
+  new_array = []
+  index = 0 
+  while index < array.length 
+    array.each { |thing| new_array << thing.capitalize }
+   end
+  index += 1 
+  new_array
 end
 
 def long_planeteer_calls# code an argument here
@@ -14,3 +22,6 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+

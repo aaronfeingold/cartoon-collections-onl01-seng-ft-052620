f372ee52d203425a65211a0c6855df81c055120f
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 00:15:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,7 @@ def find_the_cheese (array)# code an argument here
   array.select { |thing| 
     if thing == "cheddar" 
       return thing
-    else  
-      return false  
+    
     end
   }
   

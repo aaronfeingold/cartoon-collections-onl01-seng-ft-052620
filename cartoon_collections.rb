60042cee22ee029f91294962558b7c9092be7188
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 00:12:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,12 @@ end
 
 def find_the_cheese (array)# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include? ("cheddar")
+  # cheese_types = ["cheddar", "gouda", "camembert"]
+  array.select { |thing| 
+    if thing == "cheddar" 
+      return thing
+    end
+  }
   
 end
 
